edc940258b4265ab2f9b07aeb27991deab0577e7
Moshood Kareem <42604147+Moshk94@users.noreply.github.com>
2022-08-15 16:32:30 +0100

    Add text and animations for title screen

--- src/core.js
+++ src/core.js
@@ -1,8 +1,11 @@
 import {ctx} from './constants.js'
-import {animateTitle} from './titlepage.js'
+import {animateTitle, animatePlayButton, animateH2PButton} from './titlepage.js'
 
 setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     animateTitle();
+    animatePlayButton();
+    animateH2PButton();
 
 }, 1/60);
