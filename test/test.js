8eab65081f6ea14ff173490d460842a0d810ab98
Moshood Kareem <42604147+Moshk94@users.noreply.github.com>
2022-08-20 13:27:47 +0100

    Add mouse events to the test evironment

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 const c = document.getElementById('canvas');
 const ctx = c.getContext("2d");
 
+let mouseX;
+let mouseY;
+
+canvas.addEventListener('mousemove', function (e) {}, false);
+
+canvas.addEventListener('mouseup', function (e) {}, false);
+
 setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
  }, 1 / 60);
